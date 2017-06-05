34acd481fe90402ef899a701293654e1ead7b324
AKI <a.okada3@gmail.com>
2017-06-04 23:11:25 -0700

    First Commit

--- JavaQ/app/src/main/java/com/example/aki/javaq/MainActivity.java
+++ JavaQ/app/src/main/java/com/example/aki/javaq/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.aki.javaq;
 
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
