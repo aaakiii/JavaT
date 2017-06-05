0c0efc0ae3c54f7b481c8710e161dd8d80af9b7e
AKI <a.okada3@gmail.com>
2017-06-04 22:49:28 -0700

    First Commit

---
+++ JavaQ/app/src/main/java/com/example/aki/javaq/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.aki.javaq;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
