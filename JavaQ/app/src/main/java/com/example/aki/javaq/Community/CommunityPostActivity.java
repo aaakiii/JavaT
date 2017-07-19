ca1aaeab8cda376a105011a020b0094eea253b79
AKI <a.okada3@gmail.com>
2017-07-18 22:09:32 -0700

    update ComPostActivity

--- JavaQ/app/src/main/java/com/example/aki/javaq/Community/CommunityPostActivity.java
+++ JavaQ/app/src/main/java/com/example/aki/javaq/Community/CommunityPostActivity.java
@@ -82,9 +82,7 @@ public class CommunityPostActivity extends AppCompatActivity {
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         //New Child entries
-        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
-
-
+        mFirebaseDatabaseReference = FirebaseLab.getFirebaseDatabaseReference();
 
         mPostButton = (Button) findViewById(R.id.post_button);
         mPostButton.setOnClickListener(new View.OnClickListener() {
@@ -101,34 +99,13 @@ public class CommunityPostActivity extends AppCompatActivity {
             }
         });
 
-        // Initialize Firebase Measurement.
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mFirebaseAnalytics = FirebaseLab.getFirebaseAnalytics(this);
+        mFirebaseRemoteConfig = FirebaseLab.getFirebaseRemoteConfig();
+        FirebaseLab.SetConfig();
+        FirebaseLab.fetchConfig();
 
-        // Initialize Firebase Remote Config.
-        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
-
-        // Define Firebase Remote Config Settings.
-        FirebaseRemoteConfigSettings firebaseRemoteConfigSettings =
-                new FirebaseRemoteConfigSettings.Builder()
-                        .setDeveloperModeEnabled(true)
-                        .build();
-
-
-        // Define default config values. Defaults are used when fetched config values are not
-        // available. Eg: if an error occurred fetching values from the server.
-        Map<String, Object> defaultConfigMap = new HashMap<>();
-        defaultConfigMap.put("friendly_msg_length", 10L);
-        // Apply config settings and default values.
-        mFirebaseRemoteConfig.setConfigSettings(firebaseRemoteConfigSettings);
-        mFirebaseRemoteConfig.setDefaults(defaultConfigMap);
-
-        // Fetch remote config.
-        fetchConfig();
-        // Fetch the config to determine the allowed length of messages.
-
-
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mFirebaseAuth = FirebaseLab.getFirebaseAuth();
+        mFirebaseUser = FirebaseLab.getFirebaseUser();
         mUsername = mFirebaseUser.getDisplayName();
 
         if(mFirebaseUser.getPhotoUrl() != null){
@@ -191,35 +168,6 @@ public class CommunityPostActivity extends AppCompatActivity {
 //    }
 
 
-    public void fetchConfig() {
-        long cacheExpiration = 3600; // 1 hour in seconds
-        // If developer mode is enabled reduce cacheExpiration to 0 so that
-        // each fetch goes to the server. This should not be used in release
-        // builds.
-        if (mFirebaseRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
-            cacheExpiration = 0;
-        }
-        mFirebaseRemoteConfig.fetch(cacheExpiration)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        // Make the fetched config available via
-                        // FirebaseRemoteConfig get<type> calls.
-                        mFirebaseRemoteConfig.activateFetched();
-                        applyRetrievedLengthLimit();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // There has been an error fetching the config
-                        Log.w(TAG, "Error fetching config: " +
-                                e.getMessage());
-                        applyRetrievedLengthLimit();
-                    }
-                });
-
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -293,13 +241,11 @@ public class CommunityPostActivity extends AppCompatActivity {
     }
 
 
-
-
-
     private void applyRetrievedLengthLimit() {
         Long friendly_msg_length = mFirebaseRemoteConfig.getLong("friendly_msg_length");
         mEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(friendly_msg_length.intValue())});
         Log.d(TAG, "FML is: " + friendly_msg_length);
+
     }
 
 }
