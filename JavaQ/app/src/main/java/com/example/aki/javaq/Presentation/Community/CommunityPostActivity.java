cd21db018d612c8b9cdfa3fc6d6aa9214fde575d
MinaFujisawa <fujimina37@gmail.com>
2017-07-19 21:45:41 -0700

    Update Post's UI

--- JavaQ/app/src/main/java/com/example/aki/javaq/Presentation/Community/CommunityPostActivity.java
+++ JavaQ/app/src/main/java/com/example/aki/javaq/Presentation/Community/CommunityPostActivity.java
@@ -1,32 +1,49 @@
 package com.example.aki.javaq.Presentation.Community;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.InputFilter;
+import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewTreeObserver;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.aki.javaq.Domain.Entity.PostMainContents;
 import com.example.aki.javaq.Domain.Helper.JavaQPreferences;
 import com.example.aki.javaq.Domain.Usecase.FirebaseLab;
+import com.example.aki.javaq.Domain.Usecase.Loading;
+import com.example.aki.javaq.Domain.Helper.PictureUtils;
+import com.example.aki.javaq.R;
 import com.google.android.gms.appinvite.AppInviteInvitation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import com.example.aki.javaq.R;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
@@ -34,20 +51,23 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.util.regex.Pattern;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by MinaFujisawa on 2017/07/13.
+ */
+
 public class CommunityPostActivity extends AppCompatActivity {
 
-    public static final String MESSAGES_CHILD = "messages";
-    private static final String TAG = "CommunityPostActivity";
-    private static final String MESSAGE_SENT_EVENT = "message_sent";
+    private MenuItem mSaveButton;
+    private boolean mTappable;
     private SharedPreferences mSharedPreferences;
-    private static final int REQUEST_INVITE = 1;
-    private static final int REQUEST_IMAGE = 2;
-    private static final String LOADING_IMAGE_URL = "https://www.google.com/images/spin-32.gif";
-    public static final int DEFAULT_MSG_LENGTH_LIMIT = 30;
+    private EditText mEditTextView;
     public static String mUsername;
-    private static final String MESSAGE_URL = "http://javaq.firebase.google.com/";
-    private Button mPostButton;
-    private EditText mEditText;
+
     private static FirebaseAuth mFirebaseAuth;
     private static FirebaseUser mFirebaseUser;
     private FirebaseRemoteConfig mFirebaseRemoteConfig;
@@ -55,35 +75,32 @@ public class CommunityPostActivity extends AppCompatActivity {
     public DatabaseReference mFirebaseDatabaseReference;
     private static String mPhotoUrl;
 
+    private static final int REQUEST_INVITE = 1;
+    private static final int REQUEST_IMAGE = 2;
+    public static final int DEFAULT_MSG_LENGTH_LIMIT = 30;
+    private static final String LOADING_IMAGE_URL = "https://www.google.com/images/spin-32.gif";
+    public static final String MESSAGES_CHILD = "messages";
+    private static final String TAG = "CommunityPostActivity";
+    private static final String MESSAGE_SENT_EVENT = "message_sent";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.com_post_activity);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_with_button);
+        setSupportActionBar(myToolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setDisplayShowTitleEnabled(false);
+        ab.setHomeAsUpIndicator(R.drawable.ic_close);
+
 
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         //New Child entries
         mFirebaseDatabaseReference = FirebaseLab.getFirebaseDatabaseReference();
-
-        mPostButton = (Button) findViewById(R.id.post_button);
-        mPostButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PostMainContents postMainContents = new PostMainContents(mEditText.getText().toString(), mUsername, false, mPhotoUrl);
-                mFirebaseDatabaseReference.child(MESSAGES_CHILD).push().setValue(postMainContents);
-                mEditText.setText("");
-                mFirebaseAnalytics.logEvent(MESSAGE_SENT_EVENT, null);
-
-                Intent intent = new Intent(CommunityPostActivity.this, CommunityListActivity.class);
-                startActivity(intent);
-
-            }
-        });
-
         mFirebaseAnalytics = FirebaseLab.getFirebaseAnalytics(this);
         mFirebaseRemoteConfig = FirebaseLab.getFirebaseRemoteConfig();
         FirebaseLab.SetConfig();
@@ -92,67 +109,39 @@ public class CommunityPostActivity extends AppCompatActivity {
         mFirebaseAuth = FirebaseLab.getFirebaseAuth();
         mFirebaseUser = FirebaseLab.getFirebaseUser();
         mUsername = mFirebaseUser.getDisplayName();
-
-        if(mFirebaseUser.getPhotoUrl() != null){
+        if (mFirebaseUser.getPhotoUrl() != null) {
             mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
         }
 
-        mEditText = (EditText) findViewById(R.id.edit_post);
-        mEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(mSharedPreferences.getInt(JavaQPreferences.FRIENDLY_MSG_LENGTH, DEFAULT_MSG_LENGTH_LIMIT))});
-        mEditText.addTextChangedListener(new TextWatcher() {
+
+        mEditTextView = (EditText) findViewById(R.id.edit_post);
+        mEditTextView.setFilters(new InputFilter[]{new InputFilter
+                .LengthFilter(mSharedPreferences
+                .getInt(JavaQPreferences
+                        .FRIENDLY_MSG_LENGTH, DEFAULT_MSG_LENGTH_LIMIT))});
+        mEditTextView.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+                //
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                invalidateOptionsMenu();
+
                 if (s.toString().trim().length() > 0) {
-                    mPostButton.setEnabled(true);
+                    mTappable = true;
                 } else {
-                    mPostButton.setEnabled(false);
+                    mTappable = false;
                 }
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-
+                //
             }
         });
     }
-
-
-
-//    public static Action getMessageViewAction(PostMainContents postMainContents){
-//        return new Action.Builder(Action.Builder.VIEW_ACTION)
-//                .setObject(postMainContents.getName(), MESSAGE_URL.concat(postMainContents.getId()))
-//                .setMetadata(new Action.Metadata.Builder().setUpload(false))
-//                .build();
-//    }
-//
-//    public static Indexable getMessageIndexable(PostMainContents postMainContents){
-//
-//        PersonBuilder sender = Indexables.personBuilder()
-//                .setIsSelf(mUsername.equals(postMainContents.getName()))
-//                .setName(postMainContents.getName())
-//                .setUrl(MESSAGE_URL.concat(postMainContents.getId() + "/sender"));
-//
-//        PersonBuilder recipient = Indexables.personBuilder()
-//                .setName(mUsername)
-//                .setUrl(MESSAGE_URL.concat(postMainContents.getId() + "/recipient"));
-//
-//        Indexable messageToIndex = Indexables.messageBuilder()
-//                .setName(postMainContents.getText())
-//                .setUrl(MESSAGE_URL.concat(postMainContents.getId()))
-//                .setSender(sender)
-//                .setRecipient(recipient)
-//                .build();
-//
-//
-//        return messageToIndex;
-//    }
-
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -189,7 +178,7 @@ public class CommunityPostActivity extends AppCompatActivity {
                             });
                 }
             }
-        }else if (requestCode == REQUEST_INVITE) {
+        } else if (requestCode == REQUEST_INVITE) {
             if (resultCode == RESULT_OK) {
                 // Use Firebase Measurement to log that invitation was sent.
                 Bundle payload = new Bundle();
@@ -210,6 +199,7 @@ public class CommunityPostActivity extends AppCompatActivity {
         }
 
     }
+
     private void putImageInStorage(StorageReference storageReference, Uri uri, final String key) {
         storageReference.putFile(uri).addOnCompleteListener(CommunityPostActivity.this,
                 new OnCompleteListener<UploadTask.TaskSnapshot>() {
@@ -223,6 +213,48 @@ public class CommunityPostActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_post, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_post:
+                PostMainContents postMainContents = new PostMainContents(mEditTextView.getText().toString(), mUsername, false, mPhotoUrl);
+                mFirebaseDatabaseReference.child(MESSAGES_CHILD).push().setValue(postMainContents);
+                mEditTextView.setText("");
+                mFirebaseAnalytics.logEvent(MESSAGE_SENT_EVENT, null);
+
+                Intent intent = new Intent(CommunityPostActivity.this, CommunityListActivity.class);
+                startActivity(intent);
+                return true;
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        mSaveButton = menu.findItem(R.id.action_post);
+        mSaveButton.setEnabled(false);
+
+        if (mTappable) {
+            mSaveButton.setEnabled(true);
+        } else {
+            mSaveButton.setEnabled(false);
+        }
+        return true;
     }
 
 }
