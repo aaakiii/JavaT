3feb32f9446443e4a4f9cc1e499d18490192a3b0
MinaFujisawa <fujimina37@gmail.com>
2017-07-20 21:25:11 -0700

    add update Name and Profile image function

--- JavaQ/app/src/main/java/com/example/aki/javaq/Domain/Entity/User.java
+++ JavaQ/app/src/main/java/com/example/aki/javaq/Domain/Entity/User.java
@@ -1,5 +1,9 @@
 package com.example.aki.javaq.Domain.Entity;
 
+import android.net.Uri;
+
+import java.net.URI;
+import java.net.URL;
 import java.util.UUID;
 
 /**
@@ -7,24 +11,26 @@ import java.util.UUID;
  */
 
 public class User {
-    private UUID mUserId;
+//    private UUID mUserId;
     private String mUserName;
-    private String mUserIcon;
+    private Uri mUserIcon;
 
+    public User() {
+    }
 
-    public User(String mUserName, String mUserIcon) {
+    public User(String mUserName, Uri mUserIcon) {
         this.mUserName = mUserName;
         this.mUserIcon = mUserIcon;
-        mUserId = UUID.randomUUID();
+//        this.mUserId = mUserId;
     }
-
-    public UUID getmUserId() {
-        return mUserId;
-    }
-
-    public void setmUserId(UUID mUserId) {
-        this.mUserId = mUserId;
-    }
+//
+//    public UUID getmUserId() {
+//        return mUserId;
+//    }
+//
+//    public void setmUserId(UUID mUserId) {
+//        this.mUserId = mUserId;
+//    }
 
     public String getmUserName() {
         return mUserName;
@@ -34,12 +40,12 @@ public class User {
         this.mUserName = mUserName;
     }
 
-    public String getmUserIcon() {
-        return "IMG_" + getmUserId().toString() + ".jpg";
-    }
+//    public String getmUserIcon() {
+//        return "IMG_" + getmUserId().toString() + ".jpg";
+//    }
 
-    public void setmUserIcon(String mUserIcon) {
-        this.mUserIcon = mUserIcon;
-    }
+//    public void setmUserIcon(URL mUserIcon) {
+//        this.mUserIcon = mUserIcon;
+//    }
 
 }
--- JavaQ/app/src/main/java/com/example/aki/javaq/Domain/Usecase/UserLab.java
+++ JavaQ/app/src/main/java/com/example/aki/javaq/Domain/Usecase/UserLab.java
@@ -1,6 +1,23 @@
 package com.example.aki.javaq.Domain.Usecase;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.example.aki.javaq.Domain.Entity.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by MinaFujisawa on 2017/07/20.
@@ -8,17 +25,34 @@ import com.example.aki.javaq.Domain.Entity.User;
 
 public class UserLab {
     public static final String USERS_CHILD = "users";
+    public static final String USER_NAME = "mUserName";
+    public DatabaseReference mDatabase;
+    private User mUser;
 
-//    public void addNewUser(String userId, String userName, String userIcon) {
-//        User user = new User(userName, userIcon);
-//        FirebaseLab.getFirebaseDatabaseReference().child(USERS_CHILD).child(userId).setValue(user);
-//    }
-    public void newChild(){
-        //New Child entries
-        FirebaseLab.SetConfig();
-        FirebaseLab.fetchConfig();
+    public UserLab() {
+//        mDatabase = FirebaseLab.getFirebaseDatabaseReference();
     }
-    public void addNewUser(User user) {
-        FirebaseLab.getFirebaseDatabaseReference().child(USERS_CHILD).child(user.getmUserId().toString()).setValue(user);
+
+    public void updateProfile(String mUserName, Uri mUserIconUri){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(mUserName)
+                .setPhotoUri(mUserIconUri)
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User profile updated.");
+                        }
+                    }
+                });
+
     }
+
+
+
 }
